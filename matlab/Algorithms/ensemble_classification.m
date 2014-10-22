017abb8fe6b530d7a49e2573f622ac76716d9739
syn <sunyanan891114@gmail.com>
2014-10-22 15:26:27 +0800

    add fcm

--- matlab/Algorithms/ensemble_classification.m
+++ matlab/Algorithms/ensemble_classification.m
@@ -17,6 +17,13 @@ elseif mapping_task(argin,'training')
             Idx = feval(Clus_name,d,k);
         case 'kcentres'
             Idx = feval(Clus_name,d,k);
+        case 'fcm'
+            [center,U,obj_fcn] = fcm(a.data,k);
+            maxU = max(U);
+            Idx = zeros(length(a.data),1);
+            for i = 1:k  
+                Idx(find(U(i,:) == maxU)) = i;
+            end            
     end
     for i = 1:1:k
         cluster{i} = find(Idx==i);
