612caf49e821fee4155e62651aa3dc4a82d2e8dc
syn <sunyanan891114@gmail.com>
2014-11-18 09:04:21 +0800

    增加selecyKbyDBCV函数，对算法mst进行修改。

--- matlab/Algorithms/RSCH_OCL_ESM_ClustFramework.m
+++ matlab/Algorithms/RSCH_OCL_ESM_ClustFramework.m
@@ -3,6 +3,7 @@
 % numClust: 如何确定聚类个数
 %       --'direct'   即直接给出聚类个数
 %       --'entropy' 利用熵确定聚类个数
+%       --'DBCV' 利用DBCV确定聚类个数
 % k: 补充参数
 %       -- 如果numClust是'direct'，则k给出具体的聚类个数
 %       -- 如果numClust是其他自动确定聚类个数的方法，则k给出聚类个数上限
@@ -20,7 +21,7 @@
 %       -- mergec 最大化决策边界集成
 
 function out = RSCH_OCL_ESM_ClustFramework(varargin)
-argin = setdefaults(varargin,[],3,'kmeans','svdd',maxc);
+argin = setdefaults(varargin,[],'direct',3,'kmeans','svdd',maxc);
 if mapping_task(argin,'definition')
 	out = define_mapping(argin,'untrained',['MLAlgo_Ensemble_' int2str(argin{2})]);
 elseif mapping_task(argin,'training')
@@ -32,9 +33,11 @@ elseif mapping_task(argin,'training')
         
     % 确定聚类个数
     if strcmp(numClust,'direct')
-        ; % 直接使用参数k
+         % 直接使用参数k
     elseif strcmp(numClust, 'entropy')
-        [k, Idx] = selectKbyEntropy(a, disM, nameClustAlgo,k);
+        [k,Idx] = selectKbyEntropy(a, disM, 'kcentres',k);
+    elseif strcmp(numClust,'DBCV')
+        [centers, Idx,k,tree,path,fdata,apts] = selectKbyDBCV(a, disM, 'kcentres',k);
     end
     
     % 调用聚类算法,聚类个数输入是k，其他参数全部设置默认值或替换到位
@@ -63,19 +66,29 @@ elseif mapping_task(argin,'training')
         end
     end
     
-    
+    if strcmp(OCCAlgo, 'mst')
+        mstdata.center = centers;
+        mstdata.tree = tree;
+        mstdata.path = path;
+        mstdata.fdata = fdata; 
+        mstdata.apts = apts;
+    else
     % 对每一个聚类簇生成单类分类模型
-    subW = cell(1,k);
-    for i = 1:1:k
-        thisClusterIdx = find(Idx==i);
-        A = seldat(a, [],[], thisClusterIdx);
-        subW{i}= eval(OCCAlgo);
+        subW = cell(1,k);
+        for i = 1:1:k
+            thisClusterIdx = find(Idx==i);
+            A = seldat(a, [],[], thisClusterIdx);
+            subW{i}= eval(OCCAlgo);
+        end
+        data.subW = subW;
     end
     
     % 构建trained的prmapping
     data.Idx = Idx;
-    data.subW = subW;
+    
     data.k = k;
+    data.OCCAlgo = OCCAlgo;
+    data.mst = mstdata;
     data.combineRule = combineRule;
     out = trained_classifier(a, data);
     
@@ -83,13 +96,28 @@ elseif mapping_task(argin,'training')
 elseif mapping_task(argin,'execution')
     [a,v] = deal(argin{1:2}); 
 	mapping = getdata(v);
-    w = mapping.subW;
-%     for i = 1:1:length(w)
-%        W{i} = w{i} * dd_normc ;
-%     end
-    combineRule = mapping.combineRule;
-    W_out = [w{:}] * combineRule;
-    out = a * W_out;
+    
+    if strcmp(mapping.OCCAlgo,'mst')
+        dist =  pdist2(a.data,mapping.mst.center,'Euclidean');
+        [mmin,Idx] = min(dist,[],2);
+        for i = 1:a.objsize
+            dist = [];
+            dist = pdist2(mapping.mst.fdata{Idx(i)},a.data(i,:),'Euclidean');
+            numerator = sum((1./dist(find(dist ~= 0))) .^ a.featsize);
+            %分子除ni-1后的-1/d次幂。
+            apts(i) = (numerator/length(mapping.mst.fdata{Idx(i)})) ^(-1/a.featsize);
+            %apts为测试集中每个点的apt值。mapping.mst.apts为训练集中点的apt值。
+            for j = 1:length(mapping.mst.fdata{Idx(i)})
+                d_reach{i}(j) = max([apts(i) mapping.mst.apts{Idx(i)}(j) dist(j)])
+            end
+        end
+        %d_reach为测试集中点到该聚类簇点的可达距离。
+    else
+        w = mapping.subW;
+        combineRule = mapping.combineRule;
+        W_out = [w{:}] * combineRule;
+        out = a * W_out;
+    end
      
 end
 end
